6ec7db57d5431b52d4aeacddf3ca9f00e28649cd
Guy Suckerman <32614636+guys1444@users.noreply.github.com>
2017-12-30 17:14:16 +0200

    Update linear_search[1].php

--- linear_search[1].php
+++ linear_search[1].php
@@ -1,10 +1,23 @@
 <?php
-function contains($v, Array $vs)
-{
-	foreach ($vs as $val) {
-		if ($v === $val) return true;
+ 
+$myArray = ["orange", "lemon", "pear", "apple", "lime"];
+ 
+print(linearSearch($myArray, "kiwi"));
+ 
+function linearSearch($array, $toFind) {
+	foreach ($array as $key=>$val) {
+		if ($val == $toFind) {
+			return $key;
+		}
 	}
-	return false;
+	return -1;
 }
+ 
+?>
 
 אלגוריתם חיפוש לינארי
+
+התוכנה מבצעת חיפוש לינארי בקו ישר על המערך הלא ממויין 
+
+אם התוכנית מוצאת את המילה במערך היא מחזירה אותה 
+אם לא היא מחזירה ערך -1
